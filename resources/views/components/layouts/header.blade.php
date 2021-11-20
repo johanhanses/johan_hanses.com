e79753e14caba02702f418a518c53e5bacbd872e
Johan Hanses <johanhanses@gmail.com>
2021-11-20 10:15:11 +0100

    add link to cv

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -2,14 +2,14 @@
     <div class="flex justify-between text-lg">
         <h1 class="font-black">johanhanses.com</h1>
 
-{{--        <nav>--}}
-{{--            <a--}}
-{{--                href="https://cv.johanhanses.com"--}}
-{{--                target="_blank"--}}
-{{--                class="hover:text-gray-400 transition-colors duration-300"--}}
-{{--            >--}}
-{{--                CV--}}
-{{--            </a>--}}
-{{--        </nav>--}}
+        <nav>
+            <a
+                href="https://cv.johanhanses.com"
+                target="_blank"
+                class="hover:text-gray-400 transition-colors duration-300"
+            >
+                CV
+            </a>
+        </nav>
     </div>
 </header>
